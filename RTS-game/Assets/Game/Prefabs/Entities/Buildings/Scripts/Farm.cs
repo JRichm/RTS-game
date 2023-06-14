048a6b1cec889ed70c4493eb73805647ecf4c21c
JRichm <jblackenmusic@gmail.com>
2023-06-13 23:47:00 -0600

    added more scripts

---
+++ RTS-game/Assets/Game/Prefabs/Entities/Buildings/Scripts/Farm.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Farm : PlayerStructure
+{
+    public UIController uiController;
+
+
+    private void Start()
+    {
+    }
+
+    public override void OnClick()
+    {
+        //base.OnClick();
+        Debug.Log("I am a " + this.structureName);
+
+        if (uiController != null)
+        {
+            Debug.Log("update UI");
+        }
+    }
+}
