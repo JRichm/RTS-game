65b8e0e9275534f7be518346176c04140eeac62a
JRichm <jblackenmusic@gmail.com>
2023-06-20 13:54:46 -0600

    structures now display available entities to spawn

--- RTS-Photon/Assets/Game/Player/Scripts/UIController.cs
+++ RTS-Photon/Assets/Game/Player/Scripts/UIController.cs
@@ -8,6 +8,13 @@ public class UIController : MonoBehaviour
     public RenderTexture renderTexture;
     public RawImage miniMapImage;
     public Canvas pauseMenu;
+    public GameObject mainButtonsParent;
+    private Button[] mainButtons;
+
+    private void Start()
+    {
+        mainButtons = mainButtonsParent.GetComponentsInChildren<Button>();
+    }
 
     private void Update()
     {
@@ -20,6 +27,27 @@ public class UIController : MonoBehaviour
     public void UpdateMainButtons(List<GameObject> buttonData)
     {
         Debug.Log(buttonData);
+
+        for (int i = 0; i < mainButtons.Length; i++)
+        {
+            Debug.Log(mainButtons[i].GetComponentInChildren<Text>().text);
+
+            if (i < buttonData.Count && buttonData[i] != null)
+            {
+                mainButtons[i].gameObject.SetActive(true);
+                // Assuming the button has an Image component
+                Image imageComponent = mainButtons[i].GetComponent<Image>();
+
+                if (imageComponent != null)
+                {
+                    // Assign the Texture2D to the sprite of the Image component
+                    imageComponent.sprite = Sprite.Create(buttonData[i].GetComponent<Entity>().entityIcon, new Rect(0, 0, buttonData[i].GetComponent<Entity>().entityIcon.width, buttonData[i].GetComponent<Entity>().entityIcon.height), Vector2.one * 0.5f);
+                }
+            } else
+            {
+                mainButtons[i].gameObject.SetActive(false);
+            }
+        }
         // loop through buttons
             // if there is button data at index
                 // update buttons to display name and image
