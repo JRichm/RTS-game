3b515ca8bae95f9dd96ca66d78615b8793fc5a88
JRichm <jblackenmusic@gmail.com>
2023-06-25 23:37:57 -0600

    added comments

--- RTS-Photon/Assets/SpawnPlayers.cs
+++ RTS-Photon/Assets/SpawnPlayers.cs
@@ -29,31 +29,51 @@ public class SpawnPlayers : MonoBehaviourPunCallbacks
 
     private IEnumerator SpawnPlayersWithDelay()
     {
-        yield return new WaitForSeconds(currentPlayerIndex  * 1f); // Adjust the delay as needed
+        // wait for one second
+        yield return new WaitForSeconds(currentPlayerIndex  * 1f);
 
+
+        // spawn player
         SpawnPlayer();
 
-        yield return new WaitForSeconds(currentPlayerIndex * 1f); // Adjust the delay as needed
+        // wait for another second
+        yield return new WaitForSeconds(currentPlayerIndex * 1f);
 
+        // start loop if there are more players to spawn
         if (currentPlayerIndex < PhotonNetwork.PlayerList.Length)
         {
+
+            // start coroutine
             StartCoroutine(SpawnPlayersWithDelay());
         }
     }
 
+    // spawn player function
     private void SpawnPlayer()
     {
+        // check amount of players against playerlist again
         if (currentPlayerIndex < PhotonNetwork.PlayerList.Length)
         {
+            // get spawn position based on player number
             Vector3 spawnPosition = spawnPositions[currentPlayerIndex];
+
+            // instantiate playerobject on photon network
             GameObject newPlayer = PhotonNetwork.Instantiate(playerPrefab.name, spawnPosition, Quaternion.identity);
+
+            // increase player number/index
             currentPlayerIndex++;
         }
     }
 
+
+    // spawn player if joining after game started
     public override void OnJoinedRoom()
     {
+
+        // get player index from playerlist length
         currentPlayerIndex = PhotonNetwork.PlayerList.Length - 1;
+
+        // spawn player
         SpawnPlayer();
     }
 }
